de9c8a516eb1fabafb146353679ae61bb962f03e
Somendra Tiwari <49708374+somendrat22@users.noreply.github.com>
2026-08-12 21:00:41 +0530

    Initial code

---
+++ src/main/java/com/acciojobs/bms_august/BmsAugustApplication.java
@@ -0,0 +1,13 @@
+package com.acciojobs.bms_august;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsAugustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsAugustApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acciojobs/bms_august/BmsAugustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acciojobs.bms_august;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmsAugustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
